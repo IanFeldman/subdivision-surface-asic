50bd5e9f44d93f01430ccc1d1e53342cba262647
Casey Avila <casey@theavilas.org>
2025-05-02 15:15:04 -0700

    Add conversion code

---
+++ stuff/obj2bin.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+def f2q(n):
+    return int(n * (1 << 16)).to_bytes(4, signed=True)
+
+def convert(filename):
+    verts = 0
+    faces = 0
+    vbytes = bytearray()
+    fbytes = bytearray()
+
+    outfile = os.path.splitext(os.path.basename(filename))[0] + ".bin"
+
+    with open(filename) as f:
+        for line in f:
+            if line[0] == "v":
+                values = line.strip().split(" ")
+                for coord in values[1:]:
+                    vbytes.extend(f2q(float(coord)))
+                verts += 1
+
+            if line[0] == "f":
+                values = line.strip().split(" ")
+                for index in values[1:]:
+                    fbytes.extend(int(index).to_bytes(4))
+                faces += 1
+
+    with open(outfile, "wb") as of:
+        of.write(verts.to_bytes(4))
+        of.write(vbytes)
+        of.write(faces.to_bytes(4))
+        of.write(fbytes)
+
+
+if len(sys.argv) != 2:
+    print("Usage: " + sys.argv[0] + " <obj file>")
+    sys.exit()
+
+convert(sys.argv[1])
