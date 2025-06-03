f9c7e89e81446e1e68098857b3be2fca5501dff9
IanFeldman <ianrfeldman@gmail.com>
2025-06-03 13:41:29 -0700

    Got spi running in tb_top

--- tests/tb_top/tb_top.sv
+++ tests/tb_top/tb_top.sv
@@ -3,10 +3,18 @@
 
 module tb_top;
 
+/* testing signals */
 logic clk;
 logic reset, busy;
 logic spi_start, spi_done, mosi, miso, sck, ss;
 logic [31:0] spi_data;
+
+/* data array - size of one quadram */
+localparam A_WIDTH = 11;
+localparam NUM_WORDS = 2**A_WIDTH;
+reg [31:0] RAM[(NUM_WORDS-1): 0];
+initial $readmemh("input.hex", RAM);
+integer i;
 
 top top (
     .clk(clk),
@@ -18,12 +26,12 @@ top top (
 );
 
 spi_master spi (
-    .rstb(reset),
+    .rstb(~reset), /* active low */
     .clk(clk),
     .mlb(1'b1),
     .start(spi_start),
     .tdat(spi_data),
-    .cdiv(2'b11),
+    .cdiv(2'b00),
     .din(miso),
     .ss(ss),
     .sck(sck),
@@ -51,8 +59,29 @@ initial begin
     reset = 1;
     #CLK_PERIOD
     reset = 0;
-    while (busy) #CLK_PERIOD;
-    #CLK_PERIOD;
+
+    /* start spi transaction */
+    spi_start = 1'b1;
+    #CLK_PERIOD
+    /* send object data */
+    spi_data = RAM[0];
+    i = 1;
+    while (i < 10) begin /* TODO: send very word in ram */
+        #CLK_PERIOD
+        if (spi_done == 1'b1) begin
+            spi_data = RAM[i];
+            i = i + 1;
+        end
+    end
+    if (spi_done == 1'b1) begin
+        spi_data = 32'hFFFFFFFF;
+    end
+    #CLK_PERIOD
+    /* wait for subsurf to finish */
+    while (busy) #CLK_PERIOD
+    #CLK_PERIOD
+    #100000
+    $writememh("output.hex", RAM);
     $finish();
 end
 
